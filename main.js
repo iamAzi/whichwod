fd1bd2164ee9eb8d4e329587583e7d47da75922a
aziwang <aziwang@tencent.com>
2022-03-01 16:45:47 +0800

    feat: ans

--- main.js
+++ main.js
@@ -4,10 +4,26 @@ const app = express();
 const hostname = '0.0.0.0';
 const port = 3001;
 
+const options = [
+  '沙拉',
+  '牛排',
+  '炒饭',
+  '日料',
+  '炒青菜',
+  '拉面',
+  '辣子鸡',
+  '炒肉',
+  '炒鸡蛋',
+  '酸奶杯',
+]
+
 app.get('/wod', (req, res) => {
   res.set('Content-Type', 'text/plain;charset=utf-8');
 
-  res.send('Run 200 meters')
+  const rand = Math.floor(Math.random() * options.length)
+  res.send(options[rand])
 })
 
-app.listen(port, hostname)
+app.listen(port, hostname, () => {
+  console.log(`server on: http://${hostname}:${port}`);
+})
